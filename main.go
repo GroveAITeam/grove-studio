26d4bdeb415aa6aac9253fc2f724f9d59c8e3528
shellphy <ai.shellphy@gmail.com>
2025-04-13 03:11:47 +0800

    去掉linux构建

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"embed"
-	"github.com/wailsapp/wails/v2/pkg/logger"
-	"github.com/wailsapp/wails/v2/pkg/options/mac"
-	"github.com/wailsapp/wails/v2/pkg/options/windows"
-	"runtime"
+    "embed"
+    "github.com/wailsapp/wails/v2/pkg/logger"
+    "github.com/wailsapp/wails/v2/pkg/options/mac"
+    "github.com/wailsapp/wails/v2/pkg/options/windows"
+    "runtime"
 
-	"github.com/wailsapp/wails/v2"
-	"github.com/wailsapp/wails/v2/pkg/options"
-	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+    "github.com/wailsapp/wails/v2"
+    "github.com/wailsapp/wails/v2/pkg/options"
+    "github.com/wailsapp/wails/v2/pkg/options/assetserver"
 )
 
 //go:embed all:frontend/dist
@@ -19,62 +19,62 @@ var assets embed.FS
 var icon []byte
 
 func main() {
-	// Create an instance of the app structure
-	app := NewApp()
+    // Create an instance of the app structure
+    app := NewApp()
 
-	// Create application with options
-	err := wails.Run(&options.App{
-		Title:             "smart-engine",
-		Width:             1024,
-		Height:            768,
-		MinWidth:          1024,
-		MinHeight:         768,
-		DisableResize:     false,
-		Fullscreen:        false,
-		Frameless:         runtime.GOOS != "darwin",
-		StartHidden:       false,
-		HideWindowOnClose: true,
-		AssetServer: &assetserver.Options{
-			Assets:     assets,
-			Handler:    nil,
-			Middleware: nil,
-		},
-		BackgroundColour: &options.RGBA{R: 255, G: 255, B: 255, A: 255},
-		Menu:             nil,
-		Logger:           nil,
-		LogLevel:         logger.WARNING,
-		OnStartup:        app.startup,
-		OnDomReady:       app.domReady,
-		OnBeforeClose:    app.beforeClose,
-		OnShutdown:       app.shutdown,
-		WindowStartState: options.Normal,
-		Bind: []interface{}{
-			app,
-		},
-		// Windows 平台特殊配置
-		Windows: &windows.Options{
-			WebviewIsTransparent: true,
-			WindowIsTranslucent:  true,
-			DisableWindowIcon:    true,
-			// DisableFramelessWindowDecorations: false,
-			WebviewUserDataPath: "",
-			BackdropType:        windows.Acrylic,
-		},
-		// Mac 平台特殊配置
-		Mac: &mac.Options{
-			TitleBar:             mac.TitleBarHidden(),
-			Appearance:           mac.NSAppearanceNameVibrantLight,
-			WebviewIsTransparent: true,
-			WindowIsTranslucent:  true,
-			About: &mac.AboutInfo{
-				Title:   "my-wails-app",
-				Message: "",
-				Icon:    icon,
-			},
-		},
-	})
+    // Create application with options
+    err := wails.Run(&options.App{
+        Title:             "smart-engine",
+        Width:             1024,
+        Height:            768,
+        MinWidth:          1024,
+        MinHeight:         768,
+        DisableResize:     false,
+        Fullscreen:        false,
+        Frameless:         runtime.GOOS != "darwin",
+        StartHidden:       false,
+        HideWindowOnClose: true,
+        AssetServer: &assetserver.Options{
+            Assets:     assets,
+            Handler:    nil,
+            Middleware: nil,
+        },
+        BackgroundColour: &options.RGBA{R: 255, G: 255, B: 255, A: 255},
+        Menu:             nil,
+        Logger:           nil,
+        LogLevel:         logger.WARNING,
+        OnStartup:        app.startup,
+        OnDomReady:       app.domReady,
+        OnBeforeClose:    app.beforeClose,
+        OnShutdown:       app.shutdown,
+        WindowStartState: options.Normal,
+        Bind: []interface{}{
+            app,
+        },
+        // Windows 平台特殊配置
+        Windows: &windows.Options{
+            WebviewIsTransparent: true,
+            WindowIsTranslucent:  true,
+            DisableWindowIcon:    true,
+            // DisableFramelessWindowDecorations: false,
+            WebviewUserDataPath: "",
+            BackdropType:        windows.Acrylic,
+        },
+        // Mac 平台特殊配置
+        Mac: &mac.Options{
+            TitleBar:             mac.TitleBarHidden(),
+            Appearance:           mac.NSAppearanceNameVibrantLight,
+            WebviewIsTransparent: true,
+            WindowIsTranslucent:  true,
+            About: &mac.AboutInfo{
+                Title:   "grove-studio",
+                Message: "本地化AI工具",
+                Icon:    icon,
+            },
+        },
+    })
 
-	if err != nil {
-		println("Error:", err.Error())
-	}
+    if err != nil {
+        println("Error:", err.Error())
+    }
 }
