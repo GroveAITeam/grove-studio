1fda66bd536f8aac5ede39bed800e586c5d07401
shellphy <ai.shellphy@gmail.com>
2025-04-17 04:24:22 +0800

    引入pinna

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,8 @@
 import {createApp} from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import './style.css';
 import router from './router/index'
 
-createApp(App).use(router).mount('#app')
+const pinia = createPinia()
+createApp(App).use(pinia).use(router).mount('#app')
